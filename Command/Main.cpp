71cd518e681bc1d728e9d29016da6893c7b83508
josip <josipkeresman@gmail.com>
2025-01-20 00:51:18 +0100

    Add demo in main for command pattern

--- Command/Main.cpp
+++ Command/Main.cpp
@@ -1,9 +1,36 @@
+#include <queue>
+
 #include "Character.h"
 #include "Move.h"
 
+void fillCommandQueue(std::queue<Command*> &commandQueue)
+{
+    Move* move1 = new Move;
+    Move* move2 = new Move;
+    Move* move3 = new Move;
+    Move* move4 = new Move;
+
+    commandQueue.push(move1);
+    commandQueue.push(move2);
+    commandQueue.push(move3);
+    commandQueue.push(move4);
+}
+
+void executeCommands(std::queue<Command*> &commandQueue, Charcater &character)
+{
+    while(!commandQueue.empty())
+    {
+        commandQueue.front()->execute(character);
+        delete commandQueue.front();
+        commandQueue.pop();
+    }
+}
+
 int main()
 {
+    std::queue<Command*> commandQueue;
+    fillCommandQueue(commandQueue);
+
     Charcater joseph("Joseph");
-    Move move;
-    move.execute(joseph);
+    executeCommands(commandQueue, joseph);
 }
